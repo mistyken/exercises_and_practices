43c72477068b77004c24f9f05eda278c364f8f10
Kenneth Chen <mistyken@gmail.com>
2020-11-30 15:13:41 -0800

    adding longest common prefix

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        count = len(strs[0])
+        for i in range(count):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != char:
+                    holder = strs[0]
+                    return holder[0, i]
+
+        return strs[0]
+
+
+if __name__ == "__main__":
+    answer = Solution()
+    print(answer.longestCommonPrefix(["flower","flow","flight"]))
